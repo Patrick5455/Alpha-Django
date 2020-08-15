2ef71d4bf7d22bb216d2fc4148e59347f6a7a31e
Patrick5455 <patrickojunde@gmail.com>
2020-08-15 17:57:54 +0100

    Add register model meetings to admin.py and setup superuser for admin.py

--- codes/meeting_planner/meetings/models.py
+++ codes/meeting_planner/meetings/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Meeting (models.Model):
-    title = models.Charfield(max_length=200)
+    title = models.CharField(max_length=200)
     date = models.DateField()
 
-    
+
