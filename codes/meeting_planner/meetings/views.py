887b152b05d97e962582e24946dd92002a47c8ce
Patrick5455 <patrickojunde@gmail.com>
2020-08-16 17:49:58 +0100

    Added rooms.html and url mappings

--- codes/meeting_planner/meetings/views.py
+++ codes/meeting_planner/meetings/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 
 from meetings.models import Meeting
+from meetings.models import Room
+
 
 # Create your views here.
 
 def detail(request, id):
     # get the meeting with a particular id
-    
-    #meeting = Meeting.objects.get(pk=id)
+
+    # meeting = Meeting.objects.get(pk=id)
 
     # to handle error, we would not use the above Django get method,rather the below
     meeting = get_object_or_404(Meeting, pk=id)
-    return render (request, "detail.html", {"meeting":meeting})
+    return render(request, "meetings/detail.html", {"meeting": meeting})
 
 
+# Add a new page that shows a list of all room objects
+
+# Create a:
+# -view
+# - template
+# - url mapping
+
+def show_room(request):
+    # rooms = Room.objects.all()
+    return render(request, "meetings/rooms.html", {"rooms": Room.objects.all})
+
+
+def show_room_detail(request, id):
+    room = get_object_or_404(Room, pk=id)
+    return render(request, "meetings/this_room.html", {"room": room})
--- codes/meeting_planner/website/views.py
+++ codes/meeting_planner/website/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime as dt
+# from meetings.models import Meeting
 from meetings.models import Meeting
- 
+
 
 # views.py handles http request
-def welcome (request):
-    #return HttpResponse("Welcome to Meeting Planner")
+def welcome(request):
+    # return HttpResponse("Welcome to Meeting Planner")
 
-# to add a template page, we return a render page:
-# it will take as first argument, request and second argument, 
-# the name of the page and it's relativepath
+    # to add a template page, we return a render page:
+    # it will take as first argument, request and second argument,
+    # the name of the page and it's relative path
 
-    return render(request, "website/welcome.html", {"message":
-    "This is a message to test django templating",
-     "num_meetings": Meeting.objects.count()}) 
+    return render(request, "website/welcome.html", {"message": "This is a message to test django templating",
+                                                    "meetings": Meeting.objects.all})
 
-def date (request):
-    return HttpResponse("This page was served at "+str(dt.now()))
+
+def date(request):
+    return HttpResponse("This page was served at " + str(dt.now()))
+
 
 # add an About page that shows some text about yourself
 
-def about (request):
+def about(request):
     return HttpResponse("My name is Patrick and I am an django developer")
