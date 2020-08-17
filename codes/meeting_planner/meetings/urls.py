4951a3d606ee0a9c0c83544a77d21855bd1caf7d
Patrick5455 <patrickojunde@gmail.com>
2020-08-17 01:46:17 +0100

    Complteted Django beginner's course

--- codes/meeting_planner/meetings/urls.py
+++ codes/meeting_planner/meetings/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:id>', views.detail, name='detail'),
     path('rooms', views.show_room, name="show_room"),
     path("rooms/<int:id>", views.show_room_detail, name="room_detail"),
+    path("new", views.new, name="new"),
 ]
--- codes/meeting_planner/meetings/views.py
+++ codes/meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 
 from meetings.models import Meeting
 from meetings.models import Room
@@ -32,5 +33,24 @@ def show_room_detail(request, id):
     room = get_object_or_404(Room, pk=id)
     return render(request, "meetings/this_room.html", {"room": room})
 
+
+#  create a class of modelform_factory
+# it can generate a new class called ModelForm for us and ModelForm class can generate an html form
+
+# it takes as argument, the model we want to base the form on.
+# the exclude = [], means we want to see all the fields of the model
+# so we are excluding an empty list
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+
 def new(request):
-    return render(request, "meetings/new.html")
+    if request.method == "POST":
+        # create an object of MeetingForm with user data (payload)
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
